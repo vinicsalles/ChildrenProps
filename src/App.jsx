5d14909afd31aa85c4769962a52646302621aa8c
vinicsalles <vsmiranda4321@gmail.com>
2024-01-19 14:42:52 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Children from './components/Children'
+
+function App() {
+  
+
+  return (
+    <>
+    <Children>
+      <h2>Esse é o segundo título</h2>
+      <h3>filho1</h3>
+      <h3>filho2</h3>
+      <h3>filho3</h3>
+
+    </Children>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Children.jsx
@@ -0,0 +1,11 @@
+const Children = ({children}) => {
+  return (
+    <div>
+        <h1>Esse Título é o Primeiro</h1>
+        {children}
+
+    </div>
+  )
+}
+
+export default Children
